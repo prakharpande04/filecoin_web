0877f3d99d5cc797734c498e9e54591f1859d21f
Prakhar Pande <pandeprakhar1801@gmail.com>
2025-07-06 21:55:02 +0530

    name updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -120,7 +120,7 @@ const Hero = () => {
               style={{ animationDelay: "0.5s" }} 
               className="section-subtitle mt-3 sm:mt-6 mb-4 sm:mb-8 leading-relaxed opacity-0 animate-fade-in text-gray-950 font-normal text-base sm:text-lg text-left"
             >
-              The humanoid companion that learns and adapts alongside you.
+              An AI agent that helps you manage your assets.
             </p>
             
             <div 
